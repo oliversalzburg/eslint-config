04caa249215214aebdfe016e8f0d76b07d0135bf
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-06-16 17:03:51 +0200

    fix: Invalid tsconfig root

--- source/index.js
+++ source/index.js
@@ -31,91 +31,95 @@ const rulesJsDoc = {
   ],
 };
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  jsdoc.configs["flat/recommended"],
-  prettierConfig,
-  {
-    ignores: ["_site/", ".yarn/", ".git/", "coverage/", "lib/", "node_modules/", "*.config.*"],
-  },
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-      },
+/**
+ * @param {string} tsconfigRootDir - The root directory of YOUR project.
+ */
+export default tsconfigRootDir =>
+  tseslint.config(
+    eslint.configs.recommended,
+    jsdoc.configs["flat/recommended"],
+    prettierConfig,
+    {
+      ignores: ["_site/", ".yarn/", ".git/", "coverage/", "lib/", "node_modules/", "*.config.*"],
     },
-    linterOptions: {
-      reportUnusedDisableDirectives: "error",
-    },
-  },
-  {
-    files: ["**/*.cjs"],
-    languageOptions: {
-      parserOptions: {
-        sourceType: "commonjs",
-      },
-    },
-    plugins: { jsdoc },
-    rules: rulesJsDoc,
-  },
-  {
-    files: ["**/*.js", "**/*.mjs"],
-    languageOptions: {
-      parserOptions: {
-        sourceType: "module",
-      },
-    },
-    plugins: { jsdoc },
-    rules: rulesJsDoc,
-  },
-  {
-    files: ["**/*.cts", "**/*.mts", "**/*.ts"],
-    extends: tseslint.configs.strictTypeChecked,
-    languageOptions: {
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: { jsdoc, tsdoc },
-    rules: {
-      "@typescript-eslint/array-type": ["error", { default: "generic" }],
-      "@typescript-eslint/no-explicit-any": [
-        "error",
-        {
-          ignoreRestArgs: true,
-        },
-      ],
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["error", { args: "none" }],
-      "@typescript-eslint/no-var-requires": "off",
-      "tsdoc/syntax": "error",
-      ...rulesJsDoc,
-      "jsdoc/check-tag-names": "off",
-      "jsdoc/no-undefined-types": "off",
-      "jsdoc/require-param-type": "off",
-      "jsdoc/require-returns-type": "off",
-      "jsdoc/valid-types": "off",
-    },
-  },
-  {
-    rules: {
-      "consistent-return": "error",
-      eqeqeq: "error",
-      "no-console": "error",
-      "no-else-return": "error",
-      "no-unused-expressions": "warn",
-      "no-use-before-define": "error",
-      "prefer-const": "error",
-      strict: ["error", "global"],
-    },
-    settings: {
-      jsdoc: {
-        tagNamePreference: {
-          group: "group",
+    {
+      languageOptions: {
+        globals: {
+          ...globals.node,
+          ...globals.browser,
         },
       },
+      linterOptions: {
+        reportUnusedDisableDirectives: "error",
+      },
     },
-  },
-);
+    {
+      files: ["**/*.cjs"],
+      languageOptions: {
+        parserOptions: {
+          sourceType: "commonjs",
+        },
+      },
+      plugins: { jsdoc },
+      rules: rulesJsDoc,
+    },
+    {
+      files: ["**/*.js", "**/*.mjs"],
+      languageOptions: {
+        parserOptions: {
+          sourceType: "module",
+        },
+      },
+      plugins: { jsdoc },
+      rules: rulesJsDoc,
+    },
+    {
+      files: ["**/*.cts", "**/*.mts", "**/*.ts"],
+      extends: tseslint.configs.strictTypeChecked,
+      languageOptions: {
+        parserOptions: {
+          project: true,
+          tsconfigRootDir,
+        },
+      },
+      plugins: { jsdoc, tsdoc },
+      rules: {
+        "@typescript-eslint/array-type": ["error", { default: "generic" }],
+        "@typescript-eslint/no-explicit-any": [
+          "error",
+          {
+            ignoreRestArgs: true,
+          },
+        ],
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["error", { args: "none" }],
+        "@typescript-eslint/no-var-requires": "off",
+        "tsdoc/syntax": "error",
+        ...rulesJsDoc,
+        "jsdoc/check-tag-names": "off",
+        "jsdoc/no-undefined-types": "off",
+        "jsdoc/require-param-type": "off",
+        "jsdoc/require-returns-type": "off",
+        "jsdoc/valid-types": "off",
+      },
+    },
+    {
+      rules: {
+        "consistent-return": "error",
+        eqeqeq: "error",
+        "no-console": "error",
+        "no-else-return": "error",
+        "no-unused-expressions": "warn",
+        "no-use-before-define": "error",
+        "prefer-const": "error",
+        strict: ["error", "global"],
+      },
+      settings: {
+        jsdoc: {
+          tagNamePreference: {
+            group: "group",
+          },
+        },
+      },
+    },
+  );
